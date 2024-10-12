e1f23eda73abf7bf2bd0bc447350f503445abb81
Debajyoti Bhakta <debajyotitech04@gmail.com>
2024-10-12 12:13:13 +0530

    Add files via upload

---
+++ First.py
@@ -0,0 +1,24 @@
+print("Hello Yash")
+print(5/4)
+"""This is for
+multiline comment"""
+# Activity 1
+
+# Method to print multi lines in single print
+print('''How happy is the little stone
+That rambles in the road alone,
+And doesn't care about careers,
+And exigencies never fears;
+Whose coat of elemental brown
+A passing universe put on;
+And independent as the sun,
+Associates or glows alone,
+Fulfilling absolute decree
+In casual simplicity.''')
+
+# Use REPL & print table of 5
+print(5*1) 
+print(5*2)
+# ...
+print(5*10)
+
