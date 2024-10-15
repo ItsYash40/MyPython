73774a043c30301a1be00a6887e0f6a4475359c8
Debajyoti Bhakta <debajyotitech04@gmail.com>
2024-10-15 23:00:46 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,21 @@
+fruits = ["Apple", "Banana", 4, 4.5, " Mango", False]
+print(fruits)
+print(fruits[1]) # print element of index 1
+
+fruits[1]= "Milk" #Unlike Strings lists are mutable
+print(fruits[1])
+
+fruits.append("graves")  #add at the end of list
+print(fruits)
+
+
+list= [3, 7, 55, 86, 4, 1, 9]
+print(list)
+
+# list.sort()  // for sorting the elements
+# list.reverse()  // for reverse printing
+# list.insert(2, 333333)  // Insert 333333 in the pos index 2
+# list.pop(1)   // pop the element of pos index 1
+# list.remove(1)  // Remove the specific element
+
+print(list)
