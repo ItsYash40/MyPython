63f08eabc70a53294e76871ea75725da5866cef1
Debajyoti Bhakta <debajyotitech04@gmail.com>
2024-10-20 23:22:29 +0530

    Add files via upload

---
+++ Conditional.py
@@ -0,0 +1,15 @@
+a= int(input("Enter your age: "))
+
+if(a>=18):
+    print("You are older")
+
+elif(a<0):
+    print("*** Age cnt be -Ve")
+
+elif(a==0):
+    print("welcome to the earth :) ")
+
+else:
+    print("NO, U r Young now")
+
+print("End the condition.")    
---
+++ nested_if.py
@@ -0,0 +1,12 @@
+a= int(input("Enter a no:"))
+
+if(a>0):
+    print("No. is +ve.")
+    if(a%2==0):
+        print("NO. is even & +ve")
+    elif(a%2!=0):
+        print("No. is odd & +ve")
+
+else:
+    print("Mo. is -Ve")        
+
