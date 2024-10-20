4d6453ba9404b1d8befcd44429cb804c4fd534a3
Debajyoti Bhakta <debajyotitech04@gmail.com>
2024-10-20 22:48:35 +0530

    Add files via upload

--- Dictionary.py
+++ Dictionary.py
@@ -8,4 +8,8 @@ print(marks["Yash"])  # mark on specific name
 # METHODS 
 print(marks.keys()) # print the key items( left side items)
 print(marks.values()) #print the values items( right side items)
+marks.update({"Nobita": 99, "Sizuka": 80}) #Update data in Dictionary
+print(marks)
 
+print(marks.get("Sneha")) #to get data 
+print(marks.pop("Allen"))
---
+++ Sets.py
@@ -0,0 +1,9 @@
+s= {32, 36, 95, 1, 1}
+
+e=set() #Don't use s={} as it will create an empty dictionary
+print(s) 
+
+s1={0,7, 4, 5, 6 }
+s2={0,1,2,3,8,9}
+print(s1.union(s2))
+print(s1.intersection(s2))
