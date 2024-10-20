2f954e03367da4a487c658f01475ecce0bd96beb
Debajyoti Bhakta <debajyotitech04@gmail.com>
2024-10-20 20:43:54 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,11 @@
+marks={"Yash": 100,
+       "Sneha": 90,
+        "Allen": 30 }
+
+print(marks,type(marks)) # for display the marks
+print(marks["Yash"])  # mark on specific name
+
+# METHODS 
+print(marks.keys()) # print the key items( left side items)
+print(marks.values()) #print the values items( right side items)
+
